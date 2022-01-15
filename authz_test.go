c7e15a1986c1d594c2cb6fff48a68cf7097ce5e7
Valentin Kuznetsov <vkuznet@gmail.com>
2022-01-15 07:25:44 -0500

    More tests

--- authz_test.go
+++ authz_test.go
@@ -12,17 +12,30 @@ func TestCheckCMSAuthz(t *testing.T) {
 	var cmsAuth CMSAuth
 	cmsAuth.Init("/etc/hosts")
 	header := make(http.Header)
-	group := "dbs"
+	group := "xcache"
 	role := "operator"
 	site := "T1"
 	res := cmsAuth.CheckCMSAuthz(header, role, group, site)
 	assert.Equal(t, res, false)
 
-	header["Cms-Authz-Operator"] = []string{"group:dbs"}
+	header["Cms-Authz-Operator"] = []string{"group:dbs group:xcache"}
+	header["Cms-Authz-Dbsexpert"] = []string{"group:dbs"}
+	header["Cms-Authz-Developer"] = []string{"group:reqmgr2"}
+	header["Cms-Authz-Admin"] = []string{"group:reqmgr group:das"}
+	header["Cms-Authz-Users"] = []string{"group:users"}
 	res = cmsAuth.CheckCMSAuthz(header, role, group, site)
 	assert.Equal(t, res, true)
 
-	header["cms-authz-operator"] = []string{"group:dbs"}
+	header["cms-authz-operator"] = []string{"group:dbs group:xcache"}
+	res = cmsAuth.CheckCMSAuthz(header, role, group, site)
+	assert.Equal(t, res, true)
+
+	site = ""
+	res = cmsAuth.CheckCMSAuthz(header, role, group, site)
+	assert.Equal(t, res, true)
+
+	header = make(http.Header)
+	header["Cms-Authz-Operator"] = []string{"group:dbs group:xcache"}
 	res = cmsAuth.CheckCMSAuthz(header, role, group, site)
 	assert.Equal(t, res, true)
 }
